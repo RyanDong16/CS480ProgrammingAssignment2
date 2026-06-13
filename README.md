e7fb087f55eb0c2dd49663cc086c5bc8235bd7e5
RyanDong16 <70402462+RyanDong16@users.noreply.github.com>
2026-06-13 12:01:00 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+## Programmers: Julie Tong and Ryan Dong
+
+## Julie Tong's Class Account Username: cssc3164
+## Ryan Dong's Class Account Username: cssc3118
+
+## Class Information: CS480, Summer 2026
+
+## Assignment Information: Programming Assignment 2
+
+## Filename: README.md
+
+
+
+# File Structure
+```
+a2/
+├── xsh.cpp
+├── xsh.exe
+├── main.cpp
+├── make
+├── QUOTE.txt
+└── README.md
+```
+
+# Compile Instructions
+
+# Operating instructions
+ // - make file
+
+
+# List/ description of novel/ significant design decisions
+
+
+
+# list/ description of any extra features/ algorithms/ functionality you included which were not required
+
+
+
+# List/ description of all known deficiencies or bugs
+
+
+
+# Lessons Learned
---
+++ shell.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstring> 
+#include <cstdlib>
+
+using namespace std;
+
+// spilts input into separate arguments for execvp()
+vector<char*> parseCommand(string& input) {
+    vector<char*> args;
+    stringstream ss(input);
+    string word;
+
+    // Split the input by spaces
+    while (ss >> word) {
+        // create string large enough to hold the word + 1
+        char* arg = new char[word.length() + 1];
+
+        // copy string as a char* argument
+        strcpy(arg, word.c_str());
+
+        // combine argument with argument list
+        args.push_back(arg);
+    }
+
+    // add nullptr to end of argument array due to using execvp()
+    args.push_back(nullptr);
+
+    return args;
+}
+
+int main() {
+    string input;
+
+    // grab the current username from the USER environment variable
+    const char* user = getenv("USER");
+
+    // if user is nullptr, use 'xsh' as the default prompt username
+    if (user == nullptr) {
+        user = "xsh";
+    }
+
+    // infinite loop that keeps the shell running
+    while (true) {
+        cout << user << "%";
+        getline(cin, input);
+
+        // If user doesn't input anything, prompt will show up again
+        if (input.empty()) {
+            continue;
+        }
+
+        // 'exit' will exit break of the infinite loop and the shell
+        if (input == "exit") {
+            break;
+        }
+        
+        // change user's input into an argument list
+        vector<char*> args = parseCommand(input);
+
+        // create a new process
+        pid_t pid = fork();
+
+        // If fork() returns a negative number, process creation failed
+        if (pid < 0) {
+            perror("fork failed");
+        } 
+        // If fork() returns 0, this code is running inside the child process
+
+
+        // if fork() returns 0, code running inside child process
+        else if (pid == 0) {
+            // Replace the child process with the command the user typed
+            if (execvp(args[0], args.data()) == -1) {
+                perror("execvp failed");
+            }
+
+            exit(1);
+        } 
+
+        // if fork() returns a positive number, code is running
+        else {
+            // waiting for child process to finish before prompt shows up
+            waitpid(pid, nullptr, 0);
+        }
+
+        // delete memory from parseCommand()
+        for (char* arg : args) {
+            delete[] arg;
+        }
+    }
+    
+    return 0;
+}
