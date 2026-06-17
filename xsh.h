81669c73c31110484c5c65d7d587ff7e1afae107
RyanDong16 <70402462+RyanDong16@users.noreply.github.com>
2026-06-16 22:59:57 -0700

    Add header file xsh.h for command parsing

---
+++ xsh.h
@@ -0,0 +1,17 @@
+// Programmers: Julie Tong and Ryan Dong
+
+// Julie Tong's Class Account Username:
+// Ryan Dong's Class Account Username: cssc3118
+
+// Class Information: CS480, Summer 2026
+
+// Assignment Information: Programming Assignment 2
+
+// Filename: xsh.h
+
+#ifndef XSH_H
+#define XSH_H
+
+vector<char*> parseCommand(string& input);
+
+#endif
